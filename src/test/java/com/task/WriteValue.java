c6e4da80061375f940a08d672fee4d6526be115d
Jayavel1999 <jayavellaptop@gmail.com>
2022-07-09 19:08:59 +0530

    backupj

---
+++ src/test/java/com/task/WriteValue.java
@@ -0,0 +1,15 @@
+package com.task;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WriteValue {
+	
+	
+	File file = new File("C:\\Users\\LENOVO\\eclipse new\\ApiProject\\src\\test\\resources\\new.json");
+	ObjectMapper mapper = new ObjectMapper();
+	
+	
+
+}
